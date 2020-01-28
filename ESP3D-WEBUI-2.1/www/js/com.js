4ba31495aeae99a7f695a5f81df2edefe1006a26
mehdigoom <medhi.gay@sfr.fr>
2020-01-28 13:27:44 +0100

    Server online

--- ESP3D-WEBUI-2.1/www/js/com.js
+++ ESP3D-WEBUI-2.1/www/js/com.js
@@ -1,11 +1,13 @@
 const io = require('socket.io-client');
-// or with import syntax
-import io from 'socket.io-client';
-const socket = io('http://public.valjang.fr:2000');
-io.on('connection', (socket) => {
-    socket.emit('Imhere',"pro3");
+socket = io.connect("192.168.1.52:2000")
+
+socket.on('connect_timeout', (timeout) => {
+    alert("Error")
+  });
+
+    socket.emit('Imhere');
     socket.on('commende', function(id) {
         SendPrinterCommand(id, true);
         console.log("Commende recu:"+ id)
+        //SendPrinterCommand("M117"+" '"+id+"'", true);
     });
-  });
---
+++ ServerDom3/start.js
@@ -0,0 +1,6 @@
+var args = {
+    port: 2000
+};
+
+var Server = require('./server.js');
+var server = new Server(args);
